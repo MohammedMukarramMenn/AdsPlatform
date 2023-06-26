3a2b4c37cdea5ecdbaaead7f903eef05b95d063e
MohammedMukarramMenn <mukarrammenn@gmail.com>
2023-06-26 10:50:46 +0530

    bug

--- app/page.js
+++ app/page.js
@@ -1,14 +1,27 @@
-import React from "react";
+'use client'
+import React, { useEffect, useState } from "react";
 import HeroSection from "../app/components/HeroSection";
-import { getPosts } from "./helper/ApiUrlHelper";
 
-const Page = async () => {
-  const res = await fetch(getPosts(), { next: { revalidate: 60 } });
-  const data = await res.json();
+const Page = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("/api/proxy/posts"); // Proxy endpoint
+        const jsonData = await res.json();
+        setData(jsonData.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
-      <HeroSection posts={data.data}/>
+      <HeroSection posts={data} />
     </>
   );
 };
